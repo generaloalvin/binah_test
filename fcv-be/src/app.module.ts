3890e86e2bdd76ac4f2502d7562925b0fadab4e3
Alvin Generalo <generalo.alvin3851@gmail.com>
2025-02-10 17:06:40 +0800

    installed dep

--- fcv-be/src/app.module.ts
+++ fcv-be/src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(
+      process.env.MONGODB_URI || 'mongodb://localhost:27017/local',
+    ),
+  ],
+  controllers: [],
   providers: [AppService],
 })
 export class AppModule {}
